c79408dfaa171b482834b670ed35961d4559d7e0
mo7aa73737-netizen <mo7aa73737@gmail.com>
2025-08-21 23:10:49 +0300

    Fix Service Worker paths for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig(({ mode }) => {
           registerType: 'autoUpdate',
           workbox: {
             globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+            skipWaiting: true,
+            clientsClaim: true,
+            navigateFallback: '/v4/index.html',
+            navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
             runtimeCaching: [
               {
                 urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
